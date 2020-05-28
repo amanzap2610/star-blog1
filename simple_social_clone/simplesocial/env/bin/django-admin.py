8d96cdc395d8bc2038c90b40bdb0565443c582df
amansaxena.zapbuild@gmail.com <amansaxena.zapbuild@gmail.com>
2020-05-28 17:11:15 +0530

    first commit

---
+++ simple_social_clone/simplesocial/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/zapbuild/Desktop/retry/simple_social_clone/simplesocial/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
